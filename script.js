30121196255e8e25d314f9035596cd8b6c3f0677
azizbek2510 <azizbekaliyev1011@gmail.com>
2025-01-21 21:00:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+const userName = prompt("Введите ваше имя:");
+
+const nameElement = document.createElement("div");
+nameElement.className = "name-display";
+nameElement.textContent = `Привет, ${userName}!`;
+
+document.body.appendChild(nameElement);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const light = prompt("rang kiriting")
+// const newTag = document.createElement("body")
+// document.body.style.backgroundColor = light
+
+
+
+
+
+
+
+
+
+
+// const width = prompt("width");
+// const height = prompt("height");
+// const rang = prompt("bg");
+
+// const newTag = document.createElement("div");
+
+// newTag.style.width = '${width}px';
+// newTag.style.height = '${heght}px';
+// newTag.style.backgroundColor = rang;
+
+// document.body.appendChild(newTag)
+
+
+
+
+
+
+
+// const width = prompt("Enter width in pixels:");
+// const height = prompt("Enter height in pixels:");
+// const rang = prompt("Enter background color:");
+
+// const newTag = document.createElement("div");
+
+// newTag.style.width = `${width}px`;
+// newTag.style.height = `${height}px`;
+// newTag.style.backgroundColor = rang;
+
+// document.body.appendChild(newTag);
